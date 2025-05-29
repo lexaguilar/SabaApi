755967d8e15a3e6aa410756ec836384a4e65cba8
lex aguilar <alejandro.aguilar@eurekani.com>
2025-05-28 18:56:38 -0600

    Refactor FilialUser models and services; remove unused properties and methods to streamline code

--- Domain/ViewModels/FilialUserModel.cs
+++ Domain/ViewModels/FilialUserModel.cs
@@ -6,16 +6,12 @@ public partial class FilialUserRequestModel
 {
     public int UserId { get; set; } 
     public int FilialId { get; set; } 
-    [NotMapped]
-    public int UserId { get; set; }
+   
 }
 
-public partial class FilialUserResponseModel:FilialUserRequestModel
+public partial class FilialUserResponseModel : FilialUserRequestModel
 {
-    public DateTime CreatedAt { get; set; }
-    public int CreatedByUserId { get; set; }
-    public DateTime? EditedAt { get; set; }
-    public int? EditedByUserId { get; set; }
+    
 }
 
 public partial class FilialUserPageResponseModel
